8769dffcf30524f786d3ff64b2ed644c89b0d926
Mark Eliasen <mark.eliasen@pm.me>
2023-06-08 10:09:13 +0100

    remove obsolete commented out env variables

--- src/env.mjs
+++ src/env.mjs
@@ -10,7 +10,6 @@ export const env = createEnv({
         DATABASE_URL: z.string().url(),
         NODE_ENV: z.enum(["development", "test", "production"]),
         NEXTAUTH_SECRET: z.string(),
-        // NEXT_PUBLIC_WS_URL: z.string().url(),
     },
 
     /**
@@ -19,7 +18,6 @@ export const env = createEnv({
      * `NEXT_PUBLIC_`.
      */
     client: {
-        // NEXT_PUBLIC_WS_URL: z.string().url(),
     },
 
     /**
@@ -30,7 +28,6 @@ export const env = createEnv({
         DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        // NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
     },
     /**
      * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
