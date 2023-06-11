ac64a119b5fe5cf1acec97e8c433edd1376e6974
Mark Eliasen <mark.eliasen@pm.me>
2023-06-11 09:07:54 +0100

    fix wording for landing

--- src/components/features-highlight.tsx
+++ src/components/features-highlight.tsx
@@ -9,10 +9,9 @@ const FeatureHighlight = () => {
                         Zero-knowledge encryption
                     </dt>
                     <dd className="mt-2 text-base text-gray-300">
-                        Encryption keys and signing keys never leaves the
-                        browser. <SnikSnak bold={true} /> wanted to,
-                        <SnikSnak bold={true} /> cannot decrypt messages or
-                        identify who wrote them.
+                        Encryption keys never leaves the browser. Even if
+                        <SnikSnak/> wanted to, <SnikSnak/> cannot decrypt
+                        any messages, it all happens in the browser.
                     </dd>
                 </div>
 
@@ -21,9 +20,9 @@ const FeatureHighlight = () => {
                         Completely anonymous
                     </dt>
                     <dd className="mt-2 text-base text-gray-300">
-                        <SnikSnak bold={true} /> does not track you or require
-                        you to sign in. Signing keys never leaves your browser,
-                        so only you can identify which messages you sent.
+                        <SnikSnak/> does not track you or require you to sign up.
+                        The signing keys a stored locally in your browser, so
+                        only you can identify which messages you sent.
                     </dd>
                 </div>
 
@@ -33,7 +32,7 @@ const FeatureHighlight = () => {
                     </dt>
                     <dd className="mt-2 text-base text-gray-300">
                         Review the code, contribute, create your own version.
-                        <SnikSnak bold={true} /> is open source (MIT) <a
+                        <SnikSnak/> is open source (MIT) <a
                             className="text-logo"
                             href="https://github.com/MrEliasen/sniksnak.chat"
                         >
