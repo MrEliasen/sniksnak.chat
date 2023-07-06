2b3ec52659034dfe30669cc02bebd556e066d77d
Mark Eliasen <mark.eliasen@pm.me>
2023-07-06 13:11:08 +0100

    add new supabase connection

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
      */
     server: {
         DATABASE_URL: z.string().url(),
+        DATABASE_DIRECT_URL: z.string().url(),
         NODE_ENV: z.enum(["development", "test", "production"]),
         NEXTAUTH_SECRET: z.string(),
     },
@@ -25,6 +26,7 @@ export const env = createEnv({
      */
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
+        DATABASE_DIRECT_URL: process.env.DATABASE_DIRECT_URL,
         NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     },
